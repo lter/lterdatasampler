9506ea3027a89027ad5ff86b734fb679404a6c5f
allisonhorst <ahorst@ucsb.edu>
2021-09-24 13:57:41 -0700

    updated bison data with Gompertz model

--- data-raw/knz_bison_data.R
+++ data-raw/knz_bison_data.R
@@ -16,12 +16,16 @@ knz_url <- "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-k
 # Download the data package with metajam
 knz_download <- download_d1_data(data_url = knz_url, path = tempdir(), dir_name="knz_bison")
 
-#+ data sampling, eval=FALSE
-#+ save data, include=FALSE, eval = FALSE
 # Read in data
 knz_files <- read_d1_files(knz_download)
+
+#' ### Data preparation
+
+#+ data_prep, eval = FALSE
 knz_bison <- knz_files$data %>%
   janitor::clean_names() %>%
   select(-rec_type)
 
+#+ save_bison_data, include = FALSE, eval = FALSE
+# Save sample file
 usethis::use_data(knz_bison, overwrite = TRUE)
